2d15ebe4bf836b11f494b1698484eacd07fcc364
William Miller <wmiller@redhat.com>
2018-01-03 10:15:48 -0500

    removed ocp-master and ocp-node roles

--- roles/ocp-master/files/firewall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin.bash
-
-firewall-cmd --permanent --add-port=8053/tcp
-firewall-cmd --permanent --add-port=8053/udp
-firewall-cmd --permanent --add-port=53/udp
-firewall-cmd --permanent --add-port=53/tcp
-
-systemctl restart firewalld
